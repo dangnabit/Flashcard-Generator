2a15815b9cd7ccd44f2f0715ae98fa051e8ae869
dangnabit <dan.gabel@gmail.com>
2017-04-22 00:58:04 -0500

    Constructors

---
+++ ClozeCard.js
@@ -0,0 +1,13 @@
+function ClozeCard(answer, question) {
+    if (this instanceof ClozeCard) {
+        this.type = 'cloze';
+        this.partial = question.replace(answer, '...');
+        this.fullText = question;
+        this.cloze = answer;
+    } else {
+        return new ClozeCard(answer, question);
+
+    }
+}
+
+module.exports = ClozeCard;
